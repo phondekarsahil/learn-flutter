b367bb77db41d605e541c96d30dfa59e6803631f
Sahil Phondekar <sahil.jphondekar@gmail.com>
2022-07-27 19:36:31 +0530

    Lesson 26: Passing data between routes

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -9,15 +9,16 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  String time = "Loading...";
   void setupWorldTime() async {
     WorldTime instance = WorldTime(
         location: "Berlin", flag: "germany.png", url: "Europe/Berlin");
     await instance.getTime();
-    setState(() {
-      time = instance.time;
+    // ignore: use_build_context_synchronously
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      "location": instance.location,
+      "flag": instance.flag,
+      "time": instance.time,
     });
-    print(instance.time);
   }
 
   @override
@@ -28,10 +29,10 @@ class _LoadingState extends State<Loading> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         body: Padding(
-      padding: const EdgeInsets.all(50.0),
-      child: Text(time),
+      padding: EdgeInsets.all(50.0),
+      child: Text("Loading..."),
     ));
   }
 }
